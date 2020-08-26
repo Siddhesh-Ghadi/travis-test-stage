dd7992ee9bb05aa95997077ff947a120b3b94419
Siddhesh-Ghadi <61187612+Siddhesh-Ghadi@users.noreply.github.com>
2020-08-26 15:24:33 +0530

    Create apt-install-test.sh

---
+++ tests/apt-install-test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt update -y && sudo apt install -y emacs
+docker run -t ubuntu:18.04 bash -c 'apt update -y && apt install -y git'
+docker run -t centos:7 bash  -c 'yum update -y && yum install -y git'
