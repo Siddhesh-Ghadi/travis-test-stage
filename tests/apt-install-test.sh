dd518883d7c933e2edc36cdc1f92c3f504b7648e
Siddhesh-Ghadi <61187612+Siddhesh-Ghadi@users.noreply.github.com>
2020-08-26 15:49:54 +0530

    Add debug flag

--- tests/apt-install-test.sh
+++ tests/apt-install-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 sudo apt update -y && sudo apt install -y emacs
 docker run -t ubuntu:18.04 bash -c 'apt update -y && apt install -y git'
 docker run -t centos:7 bash  -c 'yum update -y && yum install -y git'
